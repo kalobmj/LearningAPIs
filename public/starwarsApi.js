234d4daeaf34a9615c264f7636acd022a5cf1b90
Kalob Murphy-Jones <kalobmurphyjones@gmail.com>
2025-02-24 00:53:51 -0500

    main

--- public/starwarsApi.js
+++ public/starwarsApi.js
@@ -1,2 +1,20 @@
 // check localStorage to see if data exists
 
+// cat test
+localStorage.setItem('animal', 'cat');
+const cat = localStorage.getItem('animal');
+console.log({cat})
+
+localStorage.setItem('data', 'this is our data')
+
+const data = localStorage.getItem('data');
+console.log({data})
+
+const emptyData = localStorage.getItem('no-data')
+console.log({emptyData})
+
+if (localStorage.getItem('data') != null) {
+    console.log('our data was found in localstorage, and here it is: ', data)
+} else {
+    console.error(err => err)
+}
