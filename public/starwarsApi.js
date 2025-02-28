180ac74dc33f074c7ca08a8e9359ebe700869b6e
Kalob Murphy-Jones <kalobmurphyjones@gmail.com>
2025-02-28 01:29:03 -0500

    main

--- public/starwarsApi.js
+++ public/starwarsApi.js
@@ -3,15 +3,15 @@
 // cat test
 localStorage.setItem('animal', 'cat');
 const cat = localStorage.getItem('animal');
-console.log({cat})
+console.log({ cat })
 
 localStorage.setItem('data', 'this is our data')
 
 const data = localStorage.getItem('data');
-console.log({data})
+console.log({ data })
 
 const emptyData = localStorage.getItem('no-data')
-console.log({emptyData})
+console.log({ emptyData })
 
 if (localStorage.getItem('data') != null) {
     console.log('our data was found in localstorage, and here it is: ', data)
@@ -23,7 +23,7 @@ localStorage.setItem('star-data', 123)
 
 const starDataFromLocal = localStorage.getItem('star-data')
 
-console.log({starDataFromLocal})
+console.log({ starDataFromLocal })
 
 // fetching data for star wars films
 const starwarsFilms = 'https://swapi.dev/api/films/';
@@ -38,7 +38,7 @@ fetch(starwarsFilms)
         console.log('film data', data)
 
         filmData = data.results
-        console.log({filmData})
+        console.log({ filmData })
 
         // filmDataKeys = Object.keys(data)
         // filmDataValues = Object.values(data)
@@ -50,7 +50,7 @@ fetch(starwarsFilms)
         console.log(filmData[2])
 
         filmDataKeys = Object.keys(filmData[0])
-        console.log({filmDataKeys})
+        console.log({ filmDataKeys })
         console.log(Object.keys(filmData[0]))
         console.log('filmdata 0: ', filmData[0])
 
@@ -63,9 +63,9 @@ fetch(starwarsFilms)
     })
     .catch(err => console.error(err))
 
-    // .json() -> method used on response object from fetch call. parses response body and returns js object.
-    // JSON.parse() -> parses json string and converts to JSON
-    // JSON.stringify -> converts JS object to a single string
+// .json() -> method used on response object from fetch call. parses response body and returns js object.
+// JSON.parse() -> parses json string and converts to JSON
+// JSON.stringify -> converts JS object to a single string
 
 console.log('first getting filmData: ', localStorage.getItem('filmData'))
 
@@ -79,7 +79,7 @@ const starwarsFilmDataLocal = localStorage.getItem('filmData')
 
 console.log('test')
 
-console.log({starwarsFilmDataLocal})
+console.log({ starwarsFilmDataLocal })
 
 // localStorage.removeItem(itemhere) -> removes single item 
 // localStorage.clear() -> removes all data 
@@ -90,21 +90,117 @@ console.log('our star wars filmData should be test here: ', starwarsFilmDataLoca
 console.log('our star wars filmData should be null here: ', localStorage.getItem('filmData'))
 
 // check if we have film data, if not req from backend
-if (localStorage.getItem('filmData') === null) {
-    // fetch
+// if (localStorage.getItem('filmData') === null) {
+//     // fetch
 
-    fetch('http://localhost:3000/api')
-    .then(res => res.json())
-    .then(data => {
-        console.log({data})
-        console.log('data.results: ', data.results)
-    })
-    .catch(err => console.error('Error: ', err))
+//     fetch('http://localhost:3000/api')
+//         .then(res => res.json())
+//         .then(data => {
+//             console.log({ data })
+//             console.log('data.results: ', data.results)
+//         })
+//         .catch(err => console.error('Error: ', err))
 
 
-} else {
-    // present data
+// } else {
+//     // present data
 
-    console.log('our film movie data exists :P')
+//     console.log('our film movie data exists :P')
 
-}
+// }
+
+
+
+// Attributes:
+
+// films string -- The URL root for Film resources
+// people string -- The URL root for People resources
+// planets string -- The URL root for Planet resources
+// species string -- The URL root for Species resources
+// starships string -- The URL root for Starships resources
+// vehicles string -- The URL root for Vehicles resources
+
+// HTTP/1.0 200 OK
+// Content-Type: application/json
+// {
+//     "films": "https://swapi.dev/api/films/",
+//     "people": "https://swapi.dev/api/people/",
+//     "planets": "https://swapi.dev/api/planets/",
+//     "species": "https://swapi.dev/api/species/",
+//     "starships": "https://swapi.dev/api/starships/",
+//     "vehicles": "https://swapi.dev/api/vehicles/"
+// }
+
+
+
+// Example request:
+
+// http https://swapi.dev/api/people/1/
+
+// Example response:
+
+// HTTP/1.0 200 OK
+// Content-Type: application/json
+// {
+//     "birth_year": "19 BBY",
+//     "eye_color": "Blue",
+//     "films": [
+//         "https://swapi.dev/api/films/1/",
+//         ...
+//     ],
+//     "gender": "Male",
+//     "hair_color": "Blond",
+//     "height": "172",
+//     "homeworld": "https://swapi.dev/api/planets/1/",
+//     "mass": "77",
+//     "name": "Luke Skywalker",
+//     "skin_color": "Fair",
+//     "created": "2014-12-09T13:50:51.644000Z",
+//     "edited": "2014-12-10T13:52:43.172000Z",
+//     "species": [
+//         "https://swapi.dev/api/species/1/"
+//     ],
+//     "starships": [
+//         "https://swapi.dev/api/starships/12/",
+//         ...
+//     ],
+//     "url": "https://swapi.dev/api/people/1/",
+//     "vehicles": [
+//         "https://swapi.dev/api/vehicles/14/"
+//         ...
+//     ]
+// }
+
+// endpoints: 
+// /species/ -- get all the species resources
+// /species/:id/ -- get a specific species resource
+// /species/schema/ -- view the JSON schema for this resource
+
+
+// function to fetch any data ex: 'films'
+const fetchData = (reqData) => {
+    if (localStorage.getItem('filmData') === null) {
+        return fetch(`http://localhost:3000/api/${reqData}`)
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error(`Network response not ok: ${reqData}`)
+                }
+                return res.json()
+            })
+            .then(data => {
+                console.log(`fetched reqData: ${reqData} `, data)
+                return data;
+            })
+            .catch(err => console.error(`Error fetching data: ${reqData}: `, err))
+    } else {
+        console.log(`we have our data: ${reqData} in localStorage :)`)
+    }
+};
+
+// should console.log our fetched film data in our console log. this data is from an api call made from our backend server
+fetchData('films');
+fetchData('people');
+fetchData('planets');
+fetchData('species');
+fetchData('starships');
+fetchData('vehicles');
--- servers/server1.js
+++ servers/server1.js
@@ -38,19 +38,89 @@ app.get('/newRoute', (req, res) => {
         
 //     })
 
-app.get('/api', cors(), async (req,res) => {
-    // code here
-    // res.send(data) -> this returns data to frontend
-    const starwarsFilms = 'https://swapi.dev/api/films/';
+// HTTP/1.0 200 OK
+// Content-Type: application/json
+// {
+//     "films": "https://swapi.dev/api/films/",
+//     "people": "https://swapi.dev/api/people/",
+//     "planets": "https://swapi.dev/api/planets/",
+//     "species": "https://swapi.dev/api/species/",
+//     "starships": "https://swapi.dev/api/starships/",
+//     "vehicles": "https://swapi.dev/api/vehicles/"
+// }
 
-    fetch(starwarsFilms)
+// api call films
+app.get('/api/films', cors(), async (req,res) => {
+    // res.send(data) -> this returns data to frontend
+    const films = 'https://swapi.dev/api/films/';
+    fetch(films)
     .then(res => res.json())
     .then(data => {
         console.log({data})
         res.send(data)
     })
+});
 
-})
+// api call people
+app.get('/api/people', cors(), async (req,res) => {
+    // res.send(data) -> this returns data to frontend
+    const people = 'https://swapi.dev/api/people/';
+    fetch(people)
+    .then(res => res.json())
+    .then(data => {
+        console.log({data})
+        res.send(data)
+    })
+});
+
+// api call planets
+app.get('/api/planets', cors(), async (req,res) => {
+    // res.send(data) -> this returns data to frontend
+    const planets = 'https://swapi.dev/api/planets/';
+    fetch(planets)
+    .then(res => res.json())
+    .then(data => {
+        console.log({data})
+        res.send(data)
+    })
+});
+
+// api call species
+app.get('/api/species', cors(), async (req,res) => {
+    // res.send(data) -> this returns data to frontend
+    const species = 'https://swapi.dev/api/species/';
+    fetch(species)
+    .then(res => res.json())
+    .then(data => {
+        console.log({data})
+        res.send(data)
+    })
+});
+
+// api call starships
+app.get('/api/starships', cors(), async (req,res) => {
+    // res.send(data) -> this returns data to frontend
+    const starships = 'https://swapi.dev/api/starships/';
+    fetch(starships)
+    .then(res => res.json())
+    .then(data => {
+        console.log({data})
+        res.send(data)
+    })
+});
+
+// api call vehicles
+app.get('/api/vehicles', cors(), async (req,res) => {
+    // res.send(data) -> this returns data to frontend
+    const vehicles = 'https://swapi.dev/api/vehicles/';
+    fetch(vehicles)
+    .then(res => res.json())
+    .then(data => {
+        console.log({data})
+        res.send(data)
+    })
+});
+
 
 app.listen(port, () => {
     console.log(`Example app listening on ${port}`)
